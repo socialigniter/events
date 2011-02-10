614748a7643b8a9d5e2e457d0405ec51a79f9115
Brennan Novak <brennan@reverseproductions.com>
2011-02-09 17:05:54 -0800

    fixed create page

--- controllers/home.php
+++ controllers/home.php
@@ -58,10 +58,40 @@ class Home extends Dashboard_Controller
 	
 	function create()
 	{
-		$year 	= date('Y');
-		$month = date('m');
+		$year   			= $this->uri->segment(4);
+		$month  			= $this->uri->segment(5);
+		$day				= $this->uri->segment(6);
+		$time				= date('H:i:s');
 		
-		$this->data['calendar']		= $this->events_model->generate($year, $month);
+		if (!$year)	 $year 	= date('Y');
+		if (!$month) $month = date('m');
+		if (!$day)	 $day 	= date('d');
+				
+		$this_date			= mysql_to_unix($year.'-'.$month.'-'.$day.' '.$time);
+		
+		$hour_now			= date('h');
+		$hour_start			= $hour_now + 1;
+		$hour_end			= $hour_start + 1;
+
+		$meridiem_start		= mdate('%A', $this_date);
+
+		if ($hour_start <= 12)
+		{
+			$meridiem_end		= 'PM';
+		}
+		else
+		{
+			$meridiem_end		= 'AM';			
+		}
+		
+		$this->data['now_date']		= $month.'/'.$day.'/'.$year;
+		$this->data['start_time']	= $hour_start.':00 '.$meridiem_start;
+		$this->data['end_time']		= $hour_end.':00 '.$meridiem_end;
+		$this->data['access']		= config_item('events_default_access');
+		$this->data['repeat']		= 'none';
+		
+		$this->data['calendar']		= $this->events_model->generate($year, $month);		
+		$this->data['event_create']	= $this->load->view('partials/create_event.php', $this->data, true);
 		
 		$this->render();
 	}
--- views/home/create.php
+++ views/home/create.php
@@ -1,11 +1,26 @@
-<h3>Title</h3>
+<form id="event_create_form" method="post" action="">
+    			
+	<p><input type="text" id="event_create_title" name="event_title" size="30" value="" /></p>
 
-<p><input type="text" id="event_create_title" name="event_title" size="30" value="" /></p>
+	<p>Location<br>
+		<select name="location_id">
+			<option value="1">Scout Pit</option>
+			<option value="2">Homestead</option>
+			<option value="new">+ New Location</option>
+		</select>
+	</p>
+	
+	<p>From<br> <input type="text" id="event_create_date_start" class="event_create_date" name="date_start" value="<?= $now_date ?>"> <input class="event_create_time" type="text" name="time_start" value="<?= $start_time?>"></p>
 
-<h3>Date</h3>
-
-<a href="#" id="pick_date">Pick Date</a>
-
-<?= $calendar ?>
-
-<input type="text" name="date_start" value="">
+	<p>To<br> <input type="text" id="event_create_date_end" class="event_create_date" name="date_end" value="<?= $now_date ?>"> <input class="event_create_time" type="text" name="time_end" value="<?= $end_time ?>"></p>
+	
+	<p><?= form_dropdown('repeat', config_item('access'), $repeat) ?></p>
+	
+	<p>Access<br>
+	<?= form_dropdown('access', config_item('access'), $access) ?></p>
+					
+	<p><textarea cols="28" rows="4" id="event_create_description" name="event_description"></textarea></p>
+	
+	<p><input type="submit" value="Create" /></p>
+	
+</form>
